eb16c0a4cc1d843b07315da38268e9eb91a35e70
Kazunori Kajihiro <likerichie@gmail.com>
2015-11-24 14:43:16 +0900

    Do not use docker client

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.2
 
-RUN apk add --update curl jq python docker && \
+RUN apk add --update curl jq python && \
     curl -O https://bootstrap.pypa.io/get-pip.py && \
     python get-pip.py && \
     pip install awscli && \
--- run.sh
+++ run.sh
@@ -1,23 +1,36 @@
 #!/bin/sh
+set -e
 
 ec2_instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
 ecs_local_endpoint="ecs-agent:51678/v1"
-ecs_cluster=`http_proxy= curl http://${ecs_local_endpoint}/metadata | jq .Cluster`
-container_instance_arn=`http_proxy= curl http://${ecs_local_endpoint}/metadata | jq .ContainerInstanceArn`
+ecs_cluster=`http_proxy= https_proxy= curl http://${ecs_local_endpoint}/metadata | jq .Cluster`
+container_instance_arn=`http_proxy= https_proxy= curl http://${ecs_local_endpoint}/metadata | jq .ContainerInstanceArn | cut -d / -f2`
 
 # deregister myself from ECS cluster
-aws ecs deregister-container-instance --cluster $ecs_cluster --container-instance $container_instance_arn
+aws ecs deregister-container-instance \
+    --region=$AWS_REGION \
+    --cluster $ecs_cluster \
+    --container-instance $container_instance_arn \
+    --force
 echo "Deregistered from $ecs_cluster"
 
 # detach myself from ELBs
 
 # stop ECS tasks
 echo "Stopping docker containers..."
-docker stop --time=30 $(docker ps -q)
+
+## Do not kill myself
+myid=`cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\///' | tail -n1`
+container_ids=`curl --unix-socket /var/run/docker.sock http:/containers/json | jq -r "map(select(.Id != \"$myid\")) | map(.Id) | join(\" \")"`
+
+for container_id in $container_ids
+do
+    curl -XPOST --unix-socket /var/run/docker.sock http:/containers/$container_id/stop?t=$STOP_TIMEOUT
+done
 
 # wait for tasks to be stopped
-sleep 30
+sleep $STOP_TIMEOUT
 
 # terminate myself
 echo "Terminating the instance..."
-aws ec2 terminate-instances --instance-ids $ec2_instance_id
+aws ec2 terminate-instances --region=$AWS_REGION --instance-ids $ec2_instance_id
